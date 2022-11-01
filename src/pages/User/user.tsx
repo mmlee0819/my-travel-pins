a7d71522e2a54f2459d4781a12d8944c30fe065a
mmlee0819 <mmlee0819@gmail.com>
2022-11-01 18:07:40 +0800

    Add feature of search location and render new pin

--- src/pages/User/user.tsx
+++ src/pages/User/user.tsx
@@ -1,7 +1,14 @@
 import React from "react"
+import { useState } from "react"
 import styled from "styled-components"
 import { Link } from "react-router-dom"
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
+import {
+  useJsApiLoader,
+  GoogleMap,
+  LoadScript,
+  Marker,
+  StandaloneSearchBox,
+} from "@react-google-maps/api"
 import { darkMap } from "../User/darkMap"
 
 const Wrapper = styled.div`
@@ -18,20 +25,99 @@ const BtnLink = styled(Link)`
   margin: 0 20px;
 `
 
+const SearchWrapper = styled.div`
+  position: absolute;
+  top: 80px;
+  left: 30px;
+  display: flex;
+  flex-flow: column wrap;
+  background-color: #ffffff;
+  padding: 10px;
+  height: 60%;
+  opacity: 0.6;
+  gap: 20px;
+`
+const Input = styled.input`
+  width: 300px;
+  height: 30px;
+  font-size: 15px;
+  color: #ffffff;
+  background-color: #000000;
+  border: 1px solid #000000;
+  opacity: 0.8;
+  z-index: 100;
+`
+const BtnAddPin = styled.div`
+  display: flex;
+  width: 100px;
+  height: 30px;
+  justify-content: center;
+  align-self: self-end;
+  align-items: center;
+  color: #ffffff;
+  background-color: #000000;
+  opacity: 0.8;
+  border-radius: 10px;
+  cursor: pointer;
+`
+interface Position {
+  placeId?: string | undefined
+  lat: number | undefined
+  lng: number | undefined
+}
+
 const center = {
   lat: 25.061945,
   lng: 121.5484174,
 }
 
 function User() {
+  const [newpin, setNewPin] = useState<google.maps.LatLng | Position>()
+  // const [marker, setMarker] = useState<google.maps.Marker>()
+  const [markers, setMarkers] = useState<Position[]>([
+    {
+      placeId: "00000000mmlee",
+      lat: 25.061945,
+      lng: 121.5484174,
+    },
+  ])
+  const [searchBox, setSearchBox] = useState<
+    google.maps.places.SearchBox | StandaloneSearchBox
+  >()
+  console.log("newpin", newpin)
+  console.log("markers", markers)
+
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_google_API_KEY!,
+    libraries: ["places"],
+  })
+
   const containerStyle = {
     minHeight: "100vh",
     width: "100vw",
   }
-
-  const onLoad = (marker: google.maps.Marker) => {
+  const onMkLoad = (marker: google.maps.Marker) => {
     console.log(" marker", marker)
   }
+  const onPlacesChanged = () => {
+    if (searchBox instanceof google.maps.places.SearchBox) {
+      console.log(searchBox.getPlaces())
+      const searchResult = searchBox.getPlaces()
+      if (searchResult) {
+        const newLat = searchResult[0]?.geometry?.location?.lat()
+        const newLng = searchResult[0]?.geometry?.location?.lng()
+        console.log(searchResult[0])
+        setNewPin({ lat: newLat, lng: newLng })
+        setMarkers((prev) => {
+          return [
+            ...prev,
+            { placeId: searchResult[0].place_id, lat: newLat, lng: newLng },
+          ]
+        })
+      }
+    } else console.log("失敗啦")
+  }
+  const onLoad = (ref: google.maps.places.SearchBox) => setSearchBox(ref)
 
   return (
     <>
@@ -41,10 +127,11 @@ function User() {
         <BtnLink to="/mika/my-memories">點我去user的memories列表</BtnLink>
         <BtnLink to="/mika/my-friends">點我去user的friends列表</BtnLink>
       </Wrapper>
-      <LoadScript
+      {/* <LoadScript
         googleMapsApiKey={process.env.REACT_APP_google_API_KEY!}
         libraries={["places"]}
-      >
+      > */}
+      {isLoaded ? (
         <GoogleMap
           id="my-map"
           mapTypeId="94ce067fe76ff36f"
@@ -53,9 +140,34 @@ function User() {
           zoom={2}
           options={{ draggable: true, styles: darkMap }}
         >
-          <Marker onLoad={onLoad} position={center} />
+          <SearchWrapper>
+            <StandaloneSearchBox
+              onLoad={onLoad}
+              onPlacesChanged={onPlacesChanged}
+            >
+              <Input placeholder="City, Address..."></Input>
+            </StandaloneSearchBox>
+            <BtnAddPin>Add pin</BtnAddPin>
+          </SearchWrapper>
+          {/* <Marker onLoad={onMkLoad} position={center} />
+          <Marker
+            onLoad={onMkLoad}
+            position={{ lat: 41.9027835, lng: 12.4963655 }}
+          /> */}
+          {markers?.map((marker) => {
+            return (
+              <Marker
+                key={marker.placeId}
+                onLoad={onMkLoad}
+                position={new google.maps.LatLng(marker.lat!, marker.lng)}
+              />
+            )
+          })}
         </GoogleMap>
-      </LoadScript>
+      ) : (
+        ""
+      )}
+      {/* </LoadScript> */}
     </>
   )
 }
