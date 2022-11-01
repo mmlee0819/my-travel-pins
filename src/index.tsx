1e9cefb02b422677116e8035361cde124ae7e8ff
mmlee0819 <mmlee0819@gmail.com>
2022-11-01 13:49:26 +0800

    Render initial map and pin of user

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <React.StrictMode>
+  <>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
@@ -23,7 +23,7 @@ root.render(
       </Routes>
     </BrowserRouter>
     <App />
-  </React.StrictMode>
+  </>
 )
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/User/user.tsx
+++ src/pages/User/user.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import styled from "styled-components"
 import { Link } from "react-router-dom"
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api"
+import { darkMap } from "../User/darkMap"
 
 const Wrapper = styled.div`
   display: flex;
@@ -16,14 +18,45 @@ const BtnLink = styled(Link)`
   margin: 0 20px;
 `
 
+const center = {
+  lat: 25.061945,
+  lng: 121.5484174,
+}
+
 function User() {
+  const containerStyle = {
+    minHeight: "100vh",
+    width: "100vw",
+  }
+
+  const onLoad = (marker: google.maps.Marker) => {
+    console.log(" marker", marker)
+  }
+
   return (
-    <Wrapper>
-      <Title>我是user的地圖頁</Title>
-      <BtnLink to="/">點我回首頁</BtnLink>
-      <BtnLink to="/mika/my-memories">點我去user的memories列表</BtnLink>
-      <BtnLink to="/mika/my-friends">點我去user的friends列表</BtnLink>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Title>我是user的地圖頁</Title>
+        <BtnLink to="/">點我回首頁</BtnLink>
+        <BtnLink to="/mika/my-memories">點我去user的memories列表</BtnLink>
+        <BtnLink to="/mika/my-friends">點我去user的friends列表</BtnLink>
+      </Wrapper>
+      <LoadScript
+        googleMapsApiKey={process.env.REACT_APP_google_API_KEY!}
+        libraries={["places"]}
+      >
+        <GoogleMap
+          id="my-map"
+          mapTypeId="94ce067fe76ff36f"
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={2}
+          options={{ draggable: true, styles: darkMap }}
+        >
+          <Marker onLoad={onLoad} position={center} />
+        </GoogleMap>
+      </LoadScript>
+    </>
   )
 }
 
