c588740444293585c8c9d3ccb523dd2bfbcb97eb
mmlee0819 <mmlee0819@gmail.com>
2022-11-05 14:19:57 +0800

    Add tools list toggler

--- src/pages/Utils/tools.tsx
+++ src/pages/Utils/tools.tsx
@@ -1,9 +1,15 @@
 import React from "react"
-import { useRef, useEffect } from "react"
-import { useDrag, useGesture } from "@use-gesture/react"
-import { useSpring, animated, to } from "@react-spring/web"
+import { useState } from "react"
+import { useDrag } from "@use-gesture/react"
+import { useSpring, animated } from "@react-spring/web"
 import styled from "styled-components"
 import robot from "../assets/robotic1.png"
+import currency from "../assets/whiteCurrencies.png"
+import flight from "../assets/whiteAirplane.png"
+import hsr from "../assets/whiteHSR.png"
+import train from "../assets/whiteTrain.png"
+import tripAdvisor from "../assets/tripadvisor.png"
+import weather from "../assets/whiteWeather.png"
 
 const ToolsWrapper = styled.div`
   position: absolute;
@@ -15,19 +21,46 @@ const ToolsWrapper = styled.div`
 const DragWrapper = styled(animated.div)`
   position: relative;
   will-change: transform;
-  cursor: grab;
+  background-color: #034961;
+  border-radius: 20px;
   touch-action: none;
+  cursor: grab;
 `
 
 const RobotIcon = styled.div`
-  height: 64px;
-  width: 64px;
+  height: 50px;
+  width: 50px;
   border-radius: 10%;
   background-image: url(${robot});
-
+  background-size: contain;
   cursor: grab;
 `
+
+const CurrencyIcon = styled(RobotIcon)`
+  height: 30px;
+  width: 30px;
+  margin: 10px auto;
+  background-image: url(${currency});
+  background-size: contain;
+  cursor: pointer;
+`
+const FlightIcon = styled(CurrencyIcon)`
+  background-image: url(${flight});
+`
+const TrainIcon = styled(CurrencyIcon)`
+  background-image: url(${train});
+`
+const HSRIcon = styled(CurrencyIcon)`
+  background-image: url(${hsr});
+`
+const TAIcon = styled(CurrencyIcon)`
+  background-image: url(${tripAdvisor});
+`
+const Weather = styled(CurrencyIcon)`
+  background-image: url(${weather});
+`
 function ToolsRobot() {
+  const [showTools, setShowTools] = useState(false)
   const [{ x, y }, api] = useSpring(() => ({
     x: 0,
     y: 0,
@@ -42,8 +75,28 @@ function ToolsRobot() {
 
   return (
     <ToolsWrapper>
-      <DragWrapper style={{ x, y }} {...bindDrag()}>
-        <RobotIcon />
+      <DragWrapper
+        style={{ x, y }}
+        {...bindDrag()}
+        onClick={(e) => {
+          if ((e.target as Element).id === "robotIcon") {
+            setShowTools((prev) => !prev)
+          }
+        }}
+      >
+        <RobotIcon id="robotIcon" />
+        {showTools ? (
+          <>
+            <Weather id="weatherIcon" />
+            <CurrencyIcon id="currencyIcon" />
+            <FlightIcon id="flightIcon" />
+            <HSRIcon id="hsrIcon" />
+            <TrainIcon id="trainIcon" />
+            <TAIcon id="tripAdviIcon" />
+          </>
+        ) : (
+          ""
+        )}
       </DragWrapper>
     </ToolsWrapper>
   )
