87d72da48b7442dbfe355b323b8300e2389268d8
mmlee0819 <mmlee0819@gmail.com>
2022-11-06 19:50:08 +0800

    Add feature of selectable currencies

---
+++ src/pages/Tools/currencies.tsx
@@ -0,0 +1,25 @@
+import { db, storage } from "../Utils/firebase"
+import {
+  doc,
+  setDoc,
+  updateDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore"
+import usa from "../assets/flags/usa.png"
+import japan from "../assets/flags/japan.png"
+import uk from "../assets/flags/uk.png"
+import krw from "../assets/flags/sKorea.png"
+import china from "../assets/flags/china.png"
+import taiwan from "../assets/flags/taiwan.png"
+
+export const currencies = [
+  { id: "TWD", flag: taiwan, currency: "TWD (台幣)" },
+  { id: "USD", flag: usa, currency: "USD (美金)" },
+  { id: "EUR", flag: uk, currency: "EUR (歐元)" },
+  { id: "JPY", flag: japan, currency: "JPY (日幣)" },
+  { id: "KRW", flag: krw, currency: "KRW (韓元)" },
+  { id: "CNY", flag: china, currency: "CNY (人民幣)" },
+]
--- src/pages/Utils/tools.tsx
+++ src/pages/Utils/tools.tsx
@@ -14,6 +14,7 @@ import tripAdvisor from "../assets/tripadvisor.png"
 import weather from "../assets/whiteWeather.png"
 import "/node_modules/react-grid-layout/css/styles.css"
 import "/node_modules/react-resizable/css/styles.css"
+import { currencies } from "../Tools/currencies"
 
 const ToolsWrapper = styled.div`
   position: absolute;
@@ -82,6 +83,61 @@ const GridItemWrapper = styled.div`
 const GridItemContent = styled.div`
   padding: 8px;
 `
+const BtnClick = styled.div`
+  width: 10%;
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+  background-color: #034961;
+  border-radius: 8px;
+  cursor: pointer;
+`
+const ExchangesWrapper = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  margin-left: 8px;
+`
+const ExchangesRows = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: start;
+  gap: 10%;
+  z-index: 100;
+`
+const CurrencyRow = styled(ExchangesRows)`
+  color: #000000;
+  background-color: #ffffff;
+  border: none;
+`
+const ExchangesTitle = styled.div`
+  font-size: 12px;
+  width: 100px;
+`
+const FlagImg = styled.img`
+  margin: 0 5px;
+  width: 15px;
+  height: 15px;
+`
+
+const WhiteInputArea = styled.div`
+  align-items: center;
+  line-height: 20px;
+  width: 120px;
+  height: 20px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  cursor: pointer;
+`
+const WhiteInputTitle = styled(WhiteInputArea)``
+const AmountInput = styled.input`
+  font-size: 12px;
+  line-height: 20px;
+  width: 100px;
+  height: 20px;
+  background-color: #ffffff;
+  border-radius: 5px;
+`
 
 const ResponsiveGridLayout = WidthProvider(Responsive)
 const layout = [{ i: "exchange-rate", x: 0, y: 0, w: 1, h: 1 }]
@@ -92,6 +148,18 @@ const getLayouts = () => {
 function ToolsRobot() {
   const [showTools, setShowTools] = useState(false)
   const [showExange, setShowExange] = useState(false)
+  const [showFrom, setShowFrom] = useState(false)
+  const [showTo, setShowTo] = useState(false)
+  const [selectedFrom, setSelectedFrom] = useState({
+    id: "",
+    flag: "",
+    currency: "",
+  })
+  const [selectedTo, setSelectedTo] = useState({
+    id: "",
+    flag: "",
+    currency: "",
+  })
   const [{ x, y }, api] = useSpring(() => ({
     x: 0,
     y: 0,
@@ -110,7 +178,20 @@ function ToolsRobot() {
     localStorage.setItem("grid-layout", JSON.stringify(layouts))
   }
   const today = new Date().toDateString()
-  const currentTime = new Date().toLocaleTimeString("en-US")
+
+  const getRates = async () => {
+    try {
+      const response = await fetch("https://tw.rter.info/capi.php", {
+        method: "GET",
+        headers: {
+          "Content-Type": "text/javascript",
+        },
+      })
+      console.log("response", response.json())
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <>
       <ToolsWrapper>
@@ -149,6 +230,7 @@ function ToolsRobot() {
         <GridArea>
           <ResponsiveGridLayout
             layouts={getLayouts()}
+            key="tools"
             breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
             cols={{ lg: 5, md: 4, sm: 3, xs: 2, xxs: 1 }}
             rowHeight={60}
@@ -158,7 +240,98 @@ function ToolsRobot() {
           >
             <GridItemWrapper key="exchange-rate">
               <GridItemContent>Exchange rates</GridItemContent>
-              <GridItemContent>{`Current time: ${today} ${currentTime}`}</GridItemContent>
+              <GridItemContent>{`Data updated time: ${today}`}</GridItemContent>
+              <ExchangesWrapper>
+                <ExchangesRows>
+                  <ExchangesTitle>Amount</ExchangesTitle>
+                  <ExchangesTitle>From</ExchangesTitle>
+                  <ExchangesTitle>To</ExchangesTitle>
+                </ExchangesRows>
+                <ExchangesRows>
+                  <AmountInput />
+                  <WhiteInputTitle
+                    onClick={() => {
+                      setShowFrom((prev) => !prev)
+                    }}
+                  >
+                    {selectedFrom.id === "" ? (
+                      <WhiteInputArea />
+                    ) : (
+                      <CurrencyRow>
+                        <FlagImg src={selectedFrom.flag} />
+                        {selectedFrom.currency}
+                      </CurrencyRow>
+                    )}
+                    {showFrom &&
+                      currencies &&
+                      currencies.map((item) => {
+                        return (
+                          <>
+                            <CurrencyRow
+                              key={item.id}
+                              id={item.id}
+                              onClick={(e) => {
+                                const filteredCurrency = currencies.filter(
+                                  (item) => {
+                                    return item.id === (e.target as Element).id
+                                  }
+                                )
+                                setSelectedFrom(filteredCurrency[0])
+                              }}
+                            >
+                              <FlagImg src={item.flag} />
+                              {item.currency}
+                            </CurrencyRow>
+                          </>
+                        )
+                      })}
+                  </WhiteInputTitle>
+                  <WhiteInputTitle
+                    onClick={() => {
+                      setShowTo((prev) => !prev)
+                    }}
+                  >
+                    {selectedTo.id === "" ? (
+                      <WhiteInputArea />
+                    ) : (
+                      <CurrencyRow>
+                        <FlagImg src={selectedTo.flag} />
+                        {selectedTo.currency}
+                      </CurrencyRow>
+                    )}
+                    {showTo &&
+                      currencies &&
+                      currencies.map((item) => {
+                        return (
+                          <>
+                            <CurrencyRow
+                              key={item.id}
+                              id={item.id}
+                              onClick={(e) => {
+                                const filteredCurrency = currencies.filter(
+                                  (item) => {
+                                    return item.id === (e.target as Element).id
+                                  }
+                                )
+                                setSelectedTo(filteredCurrency[0])
+                              }}
+                            >
+                              <FlagImg src={item.flag} />
+                              {item.currency}
+                            </CurrencyRow>
+                          </>
+                        )
+                      })}
+                  </WhiteInputTitle>
+                </ExchangesRows>
+              </ExchangesWrapper>
+              <BtnClick
+                onClick={() => {
+                  getRates()
+                }}
+              >
+                Click
+              </BtnClick>
             </GridItemWrapper>
           </ResponsiveGridLayout>
         </GridArea>
